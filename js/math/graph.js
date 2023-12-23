72c8c4dd0ff385bb2e4799f8b0c271a23f28ff0a
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2023-12-23 17:36:41 +0530

    Build buldings and trees

--- js/math/graph.js
+++ js/math/graph.js
@@ -13,6 +13,10 @@ class Graph {
        return new Graph(points, segments);
     }
  
+    hash() {
+       return JSON.stringify(this);
+    }
+ 
     addPoint(point) {
        this.points.push(point);
     }
